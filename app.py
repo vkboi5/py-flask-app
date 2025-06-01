30390b91ac0425598ef50639854500033e4dfd89
vkboi5 <vkirthikjan5@gmail.com>
2025-06-01 21:37:11 +0530

    done

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return jsonify(message="Hello from Flask on Render!")
+    return jsonify(message="Hello Prerna, I deployed this flask-app successfully on Render!")
 
 if __name__ == '__main__':
     app.run(debug=True)
